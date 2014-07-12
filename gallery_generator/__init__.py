b40ee94ec779a01d181d4637a34957e28898d1ca
pelson <pelson.pub@gmail.com>
2014-07-12 12:04:43 -0500

    HTML gallery generator.

--- gallery_generator/__init__.py
+++ gallery_generator/__init__.py
@@ -1,7 +1,7 @@
 import os
 from IPython.nbformat import current as nbformat
 import itertools
-from IPython.nbconvert import RSTExporter
+from IPython.nbconvert import HTMLExporter
 import io
 
 
@@ -26,10 +26,10 @@ def notebooks_by_keyword(notebook_fnames):
     return result
 
 
-def convert_to_rst(notebook, resources, target_directory):
+def convert_to_html(notebook, resources, target_directory):
     """Return a list of strings representing the rst of the given notebook."""
-    exporter = RSTExporter()
-
+    exporter = HTMLExporter(template_file='gallery_page', template_path=[os.path.dirname(__file__)])
+    print exporter.template_file
     rst, resources = exporter.from_notebook_node(notebook, resources)
     rst = rst.split('\n')
 
@@ -47,9 +47,8 @@ def convert_to_rst(notebook, resources, target_directory):
     return rst
 
 
-def notebook_rst(notebook_fname, refname, target_directory):
+def notebook_html(notebook_fname, refname, target_directory):
     notebook = read_notebook(notebook_fname)
-    template = u"""\n.. _{ref}:\n\n\n{content}\n\n"""
 
     output = os.path.join(target_directory, 'notebook_outputs')
     if not os.path.isdir(output):
@@ -57,39 +56,41 @@ def notebook_rst(notebook_fname, refname, target_directory):
     resources = dict(unique_key=refname,
                      output_files_dir='/' + output)
 
-    content = convert_to_rst(notebook, resources, output)
-    return template.format(content=u'\n'.join(content), ref=refname)
+    content = convert_to_html(notebook, resources, output)
+    return u'\n'.join(content)
 
 
 def build_gallery(examples_directory, target_directory):
     notebook_fnames = list(notebooks_in_directory(examples_directory))
-
-    template = """ * :ref:`{refname}` <thumbnail>\n"""
     
-    gallery_content = []
+    # TODO use a jinja template.
+    template = """ * {refname}<br>\n"""
+    
+    gallery_content = ['<html><body>']
 
-#     target_directory = os.path.abspath(target_directory)
     examples_directory = os.path.join(target_directory, 'gallery')
     if not os.path.isdir(examples_directory):
         os.makedirs(examples_directory)
 
-    gallery_page_fname = os.path.join(target_directory, 'gallery.rst')
+    gallery_page_fname = os.path.join(target_directory, 'gallery.html')
 
     for fname in notebook_fnames:
         name = os.path.splitext(os.path.basename(fname))[0]
-        page_content_fname = os.path.join(examples_directory,  name + '.rst')
+        page_content_fname = os.path.join(examples_directory, name + '.html')
         refname = 'gallery-{}'.format(name.replace('_', '-'))
         with open(page_content_fname, 'w') as fh:
-            fh.write(notebook_rst(fname, refname, examples_directory).encode('utf-8'))
+            fh.write(notebook_html(fname, refname, examples_directory).encode('utf-8'))
 
     for keyword, fnames in notebooks_by_keyword(notebook_fnames).items():
-        gallery_content.append('\n\nSection: {}\n{}\n\n'.format(keyword, '-' * (len(keyword) + 10)))
+        gallery_content.append('\n\n<br><br>Section: {}<br>\n{}<br><br><br>\n\n'.format(keyword, '-' * (len(keyword) + 10)))
         for fname in fnames:
             name = os.path.splitext(os.path.basename(fname))[0]
-            page_content_fname = os.path.join(examples_directory,  name + '.rst')
-            refname = 'gallery-{}'.format(name.replace('_', '-'))
+            page_content_fname = os.path.join(examples_directory, name + '.html')
+            refname = '<a href="gallery/{}.html">{}</a>'.format(name, name)
             
             gallery_content.append(template.format(refname=refname, content=os.path.basename(fname)))
+
+    gallery_content.extend(['</body>', '</html>'])
 
     with open(gallery_page_fname, 'w') as gallery_page:
         gallery_page.writelines(gallery_content)
